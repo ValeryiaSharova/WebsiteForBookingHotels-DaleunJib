adae5c23d26cf4773bb63defc5b188eecf81c34e
Aller-Forst <gominzyplay@gmail.com>
2022-02-13 00:57:30 +0300

    Routing added.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
 import Main from './pages/main/main';
+import Products from './pages/products/products';
+import LogIn from './pages/logIn/login';
 import Footer from './sharedComponents/footer';
 import Header from './sharedComponents/header';
 
 const App = () => (
   <>
     <Header />
-    <Main />
+    <Switch>
+      <Route path="/" exact component={Main} />
+      <Route path="/products" exact component={Products} />
+      <Route path="/login" exact component={LogIn} />
+    </Switch>
     <Footer />
   </>
 );
---
+++ src/pages/logIn/login.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const LogIn = () => {
+  return (
+    <div className="container section">
+      <h1>LogIn</h1>
+    </div>
+  );
+};
+
+export default LogIn;
