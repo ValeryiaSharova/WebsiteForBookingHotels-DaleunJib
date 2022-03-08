e7c66e1761e17f5d70c3fae3e7634a8c4fe0f266
Aller-Forst <gominzyplay@gmail.com>
2022-03-09 00:31:16 +0300

    Search and filter by category added to products page.

--- src/pages/products/products.jsx
+++ src/pages/products/products.jsx
@@ -1,18 +1,10 @@
 /* eslint-disable no-underscore-dangle */
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import {
-  getCategory,
-  getCategoryError,
-  getCategoryLoadingStatus,
-  loadCategory,
-} from '../../store/category';
-import {
-  getProducts,
-  getProductsError,
-  getProductsLoadingStatus,
-  loadProducts,
-} from '../../store/products';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import Accordeon from '../../sharedComponents/accordeon';
+import ProductCard from '../../sharedComponents/productCard';
+import { getCategory, getCategoryError, getCategoryLoadingStatus } from '../../store/category';
+import { getProducts, getProductsError, getProductsLoadingStatus } from '../../store/products';
 
 const Products = () => {
   const productsState = useSelector(getProducts());
@@ -21,52 +13,78 @@ const Products = () => {
   const categoryState = useSelector(getCategory());
   const categoryError = useSelector(getCategoryError());
   const categoryLoading = useSelector(getCategoryLoadingStatus());
-  const dispatch = useDispatch();
 
-  useEffect(() => {
-    dispatch(loadCategory());
-    dispatch(loadProducts());
-  }, []);
+  const [searchValue, setSearchValue] = useState('');
+  const [selectedCategory, setSelectedCategory] = useState();
+
+  const handleSearch = ({ target }) => {
+    setSearchValue(target.value);
+  };
+
+  const handleCategorySelect = category => {
+    setSelectedCategory(category);
+  };
+
+  function searchProducts(data) {
+    if (data) {
+      if (selectedCategory) {
+        const newData = [];
+        data.forEach(p => {
+          if (p.category.includes(selectedCategory)) {
+            newData.push(p);
+          }
+        });
+        return newData.filter(product =>
+          product.name.toLowerCase().includes(searchValue.toLowerCase())
+        );
+      }
+      return data.filter(product => product.name.toLowerCase().includes(searchValue.toLowerCase()));
+    }
+    return [];
+  }
+
+  const products = searchProducts(productsState);
 
   if (productsLoading || categoryLoading) {
-    return <h1>Loading...</h1>;
+    return (
+      <section className="product section container" id="products">
+        <h1>Loading...</h1>
+      </section>
+    );
   }
 
   if (productsError || categoryError) {
-    return <h3>{productsError || categoryError}</h3>;
+    return (
+      <section className="product section container">
+        <h3>{productsError || categoryError}</h3>
+      </section>
+    );
   }
 
   return (
     <section className="product section container" id="products">
-      <h2 className="section__title-center">
-        Check out our <br /> products
-      </h2>
-
+      <h2 className="section__title-center">Our products</h2>
+      <div className="footer__subscribe">
+        <input
+          type="text"
+          placeholder="Enter plant name..."
+          className="footer__input"
+          value={searchValue}
+          onChange={handleSearch}
+        />
+      </div>
+      <div>
+        <Accordeon
+          title="Categories"
+          content="Choose one category"
+          categoryProduct={categoryState}
+          onCategorySelected={handleCategorySelect}
+        />
+      </div>
       <div className="product__container grid">
-        {productsState.map(product => {
-          return (
-            <article className="product__card" key={product._id}>
-              <div className="product__circle" />
-              <div className="product-image-container">
-                <img src={product.image} alt={product.name} className="product__img" />
-              </div>
-
-              <h3 className="product__title">{product.name}</h3>
-              <div className="container-center">
-                {product.category.map(p => (
-                  <span className="product__category" key={p}>
-                    {categoryState.find(c => c._id === p).name}
-                  </span>
-                ))}
-              </div>
-              <span className="product__price">${product.price}</span>
-
-              <button className="button--flex product__button" type="button">
-                <i className="ri-shopping-bag-line" />
-              </button>
-            </article>
-          );
-        })}
+        {products.map(product => (
+          <ProductCard categoryState={categoryState} product={product} key={product._id} />
+        ))}
       </div>
     </section>
   );
