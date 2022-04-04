d29835cfde078901711819acab02de64825c8fb5
Aller-Forst <gominzyplay@gmail.com>
2022-04-04 19:30:22 +0300

    Logic for products entity was added on backend.

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -1,4 +1,41 @@
 const express = require("express");
+const Product = require("../models/Product");
+const auth = require("../middleware/auth.middleware");
 const router = express.Router({ mergeParams: true });
+
+router
+  .route("/")
+  .get(async (req, res) => {
+    try {
+      const list = await Product.find();
+      res.status(200).send(list);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "An error has occurred on the server. Try later" });
+    }
+  })
+  .post(auth, async (req, res) => {
+    try {
+      const newProduct = await Product.create({ ...req.body });
+      res.status(200).send(newProduct);
+    } catch (error) {
+      res
+        .status(500)
+        .json({ message: "An error has occurred on the server. Try later" });
+    }
+  });
+
+router.delete("/:id", auth, async (req, res) => {
+  try {
+    const { id } = req.params;
+    const removedProduct = await Product.findById(id);
+    res.status(200).send(null);
+  } catch (error) {
+    res
+      .status(500)
+      .json({ message: "An error has occurred on the server. Try later" });
+  }
+});
 
 module.exports = router;
