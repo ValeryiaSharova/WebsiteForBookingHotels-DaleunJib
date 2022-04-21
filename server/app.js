d9a6525edc21313b6b5499da1e965796a5a97243
Aller-Forst <gominzyplay@gmail.com>
2022-04-21 23:28:26 +0300

    New rules for deployment was added.

--- server/app.js
+++ server/app.js
@@ -14,11 +14,11 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cors());
 app.use("/api", routes);
 
-const PORT = config.get("port") ?? 8080;
+const PORT = process.env.PORT ?? 8080;
 
 if (process.env.NODE_ENV === "production") {
   app.use("/", express.static(path.join(__dirname, "../client", "build")));
-  app.get("/*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../client", "build", "index.html"));
   });
 }
